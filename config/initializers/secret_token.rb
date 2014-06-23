8effe0965cc1a8515eb975e9854ac3f5af5cc79d
prabhakar <undurthi.prabhakar@gmail.com>
2014-06-23 17:22:35 +0530

    create rails app, and install devise and simple_form.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DormRoom::Application.config.secret_token = '3faab89d7aa89e458d9ea9db2d042cd7b89f1b0ebe8ab8d69eddfc4327ca67c17da7bda75dfe6f8d17498df57259af068622068f087b85bc1b914e413d60064f'
